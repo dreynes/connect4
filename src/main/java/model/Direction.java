3544cf14837a69bd1308fa8e9f78e8cc306a8ac8
dreynes <d.reynes@alumnos.upm.es>
2023-10-03 15:10:11 +0200

    Fix enum Coordinate

--- src/main/java/model/Direction.java
+++ src/main/java/model/Direction.java
@@ -3,19 +3,19 @@ package model;
 
 public enum Direction {
 
-    EAST(0,1),
-    WEST(0,1),
-    NORTH(1,0),
-    SOUTH(-1,0),
+    EAST(1,0),
+    WEST(-1,0),
+    NORTH(0,1),
+    SOUTH(0,-1),
     NORTHEAST(1,1),
     SOUTHWEST(-1,-1),
-    NORTHWEST(1,-1),
-    SOUTHEAST(-1,1);
+    NORTHWEST(-1,1),
+    SOUTHEAST(1,-1);
 
     private  int verticalDirection;
     private  int horizontalDirection;
 
-    Direction(int verticalDirection, int horizontalDirection) {
+    Direction( int horizontalDirection, int verticalDirection) {
         this.horizontalDirection = horizontalDirection;
         this.verticalDirection = verticalDirection;
     }
